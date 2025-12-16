f73151fb8a35306571c19fe0a7727eff3e896a5a
muditbansal980 <muditban2008@gmail.com>
2025-12-16 21:23:03 +0530

    changed backend link

--- frontend/src/Signup.js
+++ frontend/src/Signup.js
@@ -33,7 +33,7 @@ export default function SignUp() {
 
         const data = await res.json();
         console.log(data);
-        // alert(res.status)
+        console.log(res.status)
         if(res.ok){
             navigate('/Home');
         }
